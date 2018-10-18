40f8cec92c4da041dad3d4acad86ccabd80f7219
John T. Foster <johntfosterjr@gmail.com>
2018-10-18 10:27:46 -0500

    First commit

---
+++ iterate_build.py
@@ -0,0 +1,14 @@
+from cffi import FFI
+ffibuilder = FFI()
+
+#########################
+### Add API Code Here ###
+#########################
+
+
+#########################
+###                   ###
+#########################
+
+if __name__ == "__main__":
+    ffibuilder.compile(verbose=True)
